4a36610b62a6a782855de6a83cc4b2de9760a68d
Sh031224 <1cktmdgh2@gmail.com>
2020-09-08 17:26:09 +0900

    [ADD] types

--- src/util/types/UserStoreType.ts
+++ src/util/types/UserStoreType.ts
@@ -4,3 +4,20 @@ export interface LoginResponse {
   accessToken: string;
   message: string;
 }
+
+export interface UserInfoType {
+  idx?: number;
+  user_id?: string;
+  pw?: string;
+  name?: string;
+  description?: string;
+  image?: string;
+  is_manager?: boolean;
+  joined_at?: Date;
+}
+
+export interface GetUserInfoResponse {
+  status: number;
+  message: string;
+  data: UserInfoType;
+}
