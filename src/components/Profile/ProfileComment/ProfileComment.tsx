dde765f4131e77fc98ba210ced24d07000d1dc7c
Sh031224 <1cktmdgh2@gmail.com>
2020-09-09 03:24:30 +0900

    [ADD] comment

--- src/components/Profile/ProfileComment/ProfileComment.tsx
+++ src/components/Profile/ProfileComment/ProfileComment.tsx
@@ -1,11 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import "./ProfileComment.scss";
 import { SERVER } from "../../../config/config.json";
 import { FaTelegramPlane } from "react-icons/fa";
-import { IoIosLock, IoMdCheckmarkCircleOutline } from "react-icons/io";
-import TimeCounting from "time-counting";
 import axios from "axios";
 import { UserInfoType } from "../../../util/types/UserStoreType";
+import generateURL from "../../../util/lib/generateURL";
+import { useLocation } from "react-router-dom";
+import ProfileCommentItem from "./ProfileCommentItem";
 
 interface ProfileCommentProps {
   id: string;
@@ -13,13 +14,64 @@ interface ProfileCommentProps {
 }
 
 const ProfileComment = ({ id, myInfo }: ProfileCommentProps) => {
+  const { search } = useLocation();
+
   const [comments, setComments] = useState<any>([]);
+  const [commentInput, setCommentInput] = useState<string>("");
+
+  const createComment = useCallback(async () => {
+    if (commentInput !== "") {
+      await axios.post(
+        `${SERVER}/createComment`,
+        {
+          id: Number(search.replace("?id=", "")),
+          comment: commentInput
+        },
+        {
+          headers: {
+            Authorization: "Bearer " + localStorage.getItem("token")
+          }
+        }
+      );
+      setCommentInput("");
+      getComments();
+    }
+  }, [commentInput, search]);
+
+  const commentEnter = useCallback(
+    (e: React.KeyboardEvent<HTMLInputElement>) => {
+      if (e.keyCode === 13) {
+        createComment();
+      }
+    },
+    [commentInput, createComment]
+  );
+
+  const getComments = useCallback(() => {
+    axios
+      .get(`${SERVER}/showComment?id=${search.replace("?id=", "")}`)
+      .then(async (res: any) => {
+        let commentList = res.data.comment;
+
+        const promise: Promise<any>[] = [];
+        res.data.comment.map((data: any, i: number) => {
+          promise.push(
+            axios.get(`${SERVER}/showUserInfo?id=${data.fk_user_id}`)
+          );
+        });
+        const result = await Promise.all(promise);
+
+        result.map((data: any, i: number) => {
+          commentList[i].user_image = generateURL(data.data.data[0].image);
+          commentList[i].user_name = data.data.data[0].name;
+        });
+        setComments(commentList);
+      });
+  }, [search]);
 
   useEffect(() => {
-    axios.get(`${SERVER}/showComment?id=${id}`).then((res: any) => {
-      setComments(res.data.comment);
-    });
-  }, []);
+    getComments();
+  }, [getComments, search]);
 
   return (
     <div className="profile-comment">
@@ -28,68 +80,24 @@ const ProfileComment = ({ id, myInfo }: ProfileCommentProps) => {
           <input
             className="profile-comment-create-input"
             type="text"
-            // value={commentInput}
+            value={commentInput}
             placeholder="내용을 입력해주세요."
-            // onChange={(e: ChangeEvent<HTMLInputElement>) =>
-            // setCommentInput(e.target.value)
-            // }
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setCommentInput(e.target.value)
+            }
             maxLength={255}
-            // onKeyPress={commentEnter}
+            onKeyPress={commentEnter}
           />
           <FaTelegramPlane
-            // onClick={commentCreate}
+            onClick={createComment}
             className="profile-comment-create-submit"
           />
         </div>
         {comments && (
           <>
-            {comments.map((comment: any, i: number) => {
-              return (
-                <div className="profile-comment-item" key={i}>
-                  <div className="profile-comment-box">
-                    <div className="profile-comment-box-title">
-                      {comment.fk_user_id}
-                      {comment.fk_user_id === comment.fk_object_id && (
-                        <IoMdCheckmarkCircleOutline className="profile-comment-box-title-admin" />
-                      )}
-                      {comment.is_private && (
-                        <IoIosLock className="profile-comment-box-title-lock" />
-                      )}
-                      <span className="profile-comment-box-time">
-                        {TimeCounting(comment.created_at, { lang: "ko" })}
-                      </span>
-                    </div>
-                    <span className="profile-comment-box-content">
-                      {comment.comment}
-                    </span>
-                    <div className="profile-comment-box-util">
-                      <span
-                        className="profile-comment-box-util-reply"
-                        // onClick={() => setReply(true)}
-                      >
-                        답글
-                      </span>
-                      {comment.fk_user_id === myInfo.user_id && (
-                        <span
-                          className="profile-comment-box-util-modify"
-                          // onClick={() => setModify(true)}
-                        >
-                          수정
-                        </span>
-                      )}
-                      {comment.fk_user_id === myInfo.user_id && (
-                        <span
-                          className="profile-comment-box-util-delete"
-                          // onClick={() => deleteComment(comment.idx)}
-                        >
-                          삭제
-                        </span>
-                      )}
-                    </div>
-                  </div>
-                </div>
-              );
-            })}
+            {comments.map((comment: any, i: number) => (
+              <ProfileCommentItem comment={comment} myInfo={myInfo} key={i} />
+            ))}
           </>
         )}
       </div>
